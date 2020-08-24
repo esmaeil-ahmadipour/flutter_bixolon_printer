606c6c14ebd42a1885abf56356b319a076915d3c
Esmaeil Ahmadipour <esmaeil-ahmadipour@users.noreply.github.com>
2020-08-24 07:45:44 +0430

    Initial commit

---
+++ android/app/src/main/java/com/example/flutter_bixolon_printer/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_bixolon_printer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bixolon_printer/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
