cd8e1cf2fda0678a6a524bd809bb8b02f06d2d7c
Esmaeil Ahmadipour <esmaeil-ahmadipour@users.noreply.github.com>
2020-08-24 11:43:16 +0430

    PlatformChannel incrementCounter (Fix Flutter-Code Side)

--- lib/home.dart
+++ lib/home.dart
@@ -12,19 +12,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int counter = 0;
+  int result;
   static const platform = const MethodChannel("ir.ea2.flutter_bixolon_printer");
 
-  void incrementCounter() async {
-    int value;
+  incrementCounter() async {
     try {
-      value = await platform.invokeMethod("incrementCounter",{"counter":counter});
-      setState(() async {
-        counter=value;
-      });
+      result=await platform.invokeMethod("incrementCounter",{"counter":counter});
+      print('DartCode Result>> counter is: $result');
 
-    } catch (e) {
+    }  catch (e) {
+      print(e);
+
     }
-    print('counter is: $counter');
+
+    setState(()  {
+      if(mounted){
+        counter=result;
+      }
+
+    });
   }
 
   @override
