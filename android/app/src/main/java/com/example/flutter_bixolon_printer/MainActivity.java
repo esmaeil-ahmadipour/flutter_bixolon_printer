1ec407a32c705c78cb71db42a4eac0106e11b770
Esmaeil Ahmadipour <esmaeil-ahmadipour@users.noreply.github.com>
2020-08-24 11:42:47 +0430

    PlatformChannel incrementCounter (Set Android-JavaCode Side)

--- android/app/src/main/java/com/example/flutter_bixolon_printer/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_bixolon_printer/MainActivity.java
@@ -1,6 +1,39 @@
 package com.example.flutter_bixolon_printer;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    int counter;
+    int selectedNumber=1;
+
+    private static final String CHANNEL = "ir.ea2.flutter_bixolon_printer";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler((call, result) -> {
+
+
+            if (call.method.equals("incrementCounter")) {
+
+                counter = call.argument("counter");
+
+                if (counter != -1) {
+                    result.success(counter+selectedNumber);
+                    Log.d("JavaCode Result>> ", "counter is : " + counter);
+
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null);
+                }
+            } else {
+                result.notImplemented();
+            }
+        });
+    }
 }
